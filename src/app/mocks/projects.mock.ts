bd2367bc698bd7dc1f9eeefe02cf710ace9a9384
Alejandro <alejandro.rv97@gmail.com>
2025-12-08 22:38:16 -0500

    ADD: Text from projects fixed

--- src/app/mocks/projects.mock.ts
+++ src/app/mocks/projects.mock.ts
@@ -3,7 +3,7 @@ import { Project } from "../shared/interfaces/project.interface";
 export const projects: Project[] = [
     {
         name: 'Ruta404 Adventures',
-        description: 'A webpage for a motorcycling adventures youtube channel',
+        description: 'A static webpage in Angular, Angular Material and SASS, for a morotcycling youtube channel',
         isAlive: true,
         imageUrl: '/images/ruta404.png',
         githubUrl: 'https://github.com/lejodev/ruta-404',
@@ -17,11 +17,11 @@ export const projects: Project[] = [
         githubUrl: 'https://github.com/lejodev/new-light-backend',
         url: 'https://newlight-store-frontend.vercel.app/'
     },
-    {
-        name: 'JournalIT',
-        description: 'A Journaling website where each user can save theirn own notes, thinkings and more, is aimed to be a mobile app as well',
-        isAlive: false,
-        imageUrl: '/images/journal.png',
-        githubUrl: 'https://github.com/lejodev/journaling-frontend',
-    }
+    // {
+    //     name: 'JournalIT',
+    //     description: 'A Journaling website where each user can save theirn own notes, thinkings and more, is aimed to be a mobile app as well',
+    //     isAlive: false,
+    //     imageUrl: '/images/journal.png',
+    //     githubUrl: 'https://github.com/lejodev/journaling-frontend',
+    // }
 ]
